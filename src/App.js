d14076ea4be2c4c30228f2e0b8603b12c943b5d3
kkyoumssi <getRandomText>
2022-09-30 16:22:24 +0900

    다음맵

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DaumPostcode from "react-daum-postcode";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+
+    const handleComplete = async (v) => {
+
+        await axios.post('https://test-api.gftms.com/api/td/chkZipcode', JSON.stringify([v.zonecode]), {
+            headers:
+                {
+                    "Content-Type": `application/json`
+                    , 'TMS-APP-CODE': 'HOMEPICK'
+                    , 'TMS-APP-KEY': '38e0bc42-e060-11eb-ba80-0242ac130004'
+                }
+        }).then((res) => {
+            if (res.data.success) {
+                for (let [key, value] of Object.entries(res.data.data)) {
+                    let msg = '우편번호(' + key + ')는 ' + (value ? '배송 가능 지역입니다.' : '배송 불가능 지역입니다.')
+                    alert(msg);
+
+                }
+            }
+
+            window.location.replace("/");
+
+        }).catch((error) => {
+            console.log(error)
+        })
+
+    };
+
+    const handleSearch = () => {
+
+    };
+
+    return (
+        <DaumPostcode
+            onComplete={handleComplete}
+            onSearch={handleSearch}
+            {...props}
+        />
+    );
 }
+
+App.defaultProps = {
+    style: {
+        width: "700px",
+        height: "450px",
+    },
+};
 
 export default App;
